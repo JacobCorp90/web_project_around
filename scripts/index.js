f6fd8bbf93adff9ae330d1e8fe3afe1a1760d93f
JacoboC <jacobocortes90@hotmail.com>
2025-08-26 11:42:34 -0500

    Proyecto Sprint 8 _ Función de adicionar tarjetas añadida

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,4 @@
-const editButton = document.querySelector(".header__edit-button");
-const popup = document.querySelector(".popup");
-const closeButton = document.querySelector(".popup__close-button");
-const nameInput = document.querySelector(".popup__input_name");
-const aboutInput = document.querySelector(".popup__input_about");
-const nameDisplay = document.querySelector(".header__title");
-const aboutDisplay = document.querySelector(".header__subtitle");
-const formElement = document.querySelector(".popup__container");
-
-
-
+// Inicializa el array de tarjetas con los datos proporcionados//
 
 const initialCards = [
   {
@@ -42,18 +32,15 @@ const initialCards = [
     alt: "Lago di Braies",
   },
 ];
-// Inicializa el array de tarjetas con los datos proporcionados//
 
+const templateGalleryItem = document.querySelector("#card-template").content;
+const gallery = document.querySelector(".main__gallery");
 
-  const templateGalleryItem = document.querySelector("#card-template").content;
-  const gallery = document.querySelector(".main__gallery");
-
-  initialCards.forEach(card => {
+initialCards.forEach((card) => {
   const cardElement = templateGalleryItem.cloneNode(true);
 
   const nameImage = cardElement.querySelector(".main__gallery-photo-title");
   const linkImage = cardElement.querySelector(".main__gallery-image");
-
 
   nameImage.textContent = card.name;
   linkImage.src = card.link;
@@ -62,44 +49,75 @@ const initialCards = [
   gallery.append(cardElement);
 });
 
-// Botón para editar //
+// Botón para editar perfil //
+const editButton = document.querySelector(".header__edit-button");
+const popup = document.querySelector(".popup");
+
+const nameInput = document.querySelector(".popup__input_name");
+const aboutInput = document.querySelector(".popup__input_about");
+const nameDisplay = document.querySelector(".header__title");
+const aboutDisplay = document.querySelector(".header__subtitle");
+
 editButton.addEventListener("click", function () {
   nameInput.placeholder = nameDisplay.textContent;
   aboutInput.placeholder = aboutDisplay.textContent;
-  popup.classList.remove("popup_opened");
+  popup.classList.remove("popup_closed");
 });
 // Botón para cerrar el popup //
+const closeButton = document.querySelector(".popup__close-button");
 closeButton.addEventListener("click", function () {
-  popup.classList.add("popup_opened");
+  popup.classList.add("popup_closed");
 });
 // Función para manejar el envío del formulario//
+const formElement = document.querySelector(".popup__container");
+formElement.addEventListener("submit", handleProfileFormSubmit);
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
 
   nameDisplay.textContent = nameInput.value;
   aboutDisplay.textContent = aboutInput.value;
-  popup.classList.add("popup_opened");
+  popup.classList.add("popup_closed");
   nameInput.value = "";
   aboutInput.value = "";
 }
-// Conecta el manipulador (handler) al formulario//
-formElement.addEventListener("submit", handleProfileFormSubmit);
-
-// Pop Up nueva imagen //
-
-const imagePopup = document.querySelector(".image-popup");
-const imageCloseButton = document.querySelector(".image-popup__close-button");
-const titleInput = document.querySelector(".image-popup__input_title");
-const urlInput = document.querySelector(".image-popup__input_url");
-
 
 // Botón para agregar imagen //
 
-
+const imagePopup = document.querySelector(".image-popup");
 const addButton = document.querySelector(".header__add-button");
 addButton.addEventListener("click", function () {
-  titleInput.placeholder = nameDisplay.textContent;
-  aboutInput.placeholder = aboutDisplay.textContent;
-  popup.classList.remove("image-popup_opened");
+  imagePopup.classList.remove("image-popup_closed");
 });
 
+// Pop Up nueva imagen //
+
+const titleInput = document.querySelector(".image-popup__input_title");
+const urlInput = document.querySelector(".image-popup__input_url");
+const imageFormElement = document.querySelector(".image-popup__container");
+imageFormElement.addEventListener("submit", handleImageFormSubmit);
+
+function handleImageFormSubmit(evt) {
+  evt.preventDefault();
+
+  const cardElement = templateGalleryItem.cloneNode(true);
+  const nameImage = cardElement.querySelector(".main__gallery-photo-title");
+  const linkImage = cardElement.querySelector(".main__gallery-image");
+
+  nameImage.textContent = titleInput.value;
+  linkImage.src = urlInput.value;
+  linkImage.alt = titleInput.value;
+
+  gallery.prepend(cardElement);
+  imagePopup.classList.add("image-popup_closed");
+
+  titleInput.value = "";
+  urlInput.value = "";
+}
+
+
+// Botón para cerrar el popup de agregar imagen //
+
+const imageCloseButton = document.querySelector(".image-popup__close-button");
+imageCloseButton.addEventListener("click", function () {
+  imagePopup.classList.add("image-popup_closed");
+});
